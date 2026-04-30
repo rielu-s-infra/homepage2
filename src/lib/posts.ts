0fb5a39b72bb9fdb30f59eb8f3a7d9b7127e39c1
ぺんち <namekotoyuki100@gmail.com>
2026-04-30 18:43:49 +0900

    fix :  system

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -8,7 +8,7 @@ const postModules = import.meta.glob("/posts/*.md", {
   eager: true,
 });
 
-const contentModules = import.meta.glob("public/about/about.md", {
+const contentModules = import.meta.glob("public/about/*.md", {
   query: "?raw",
   import: "default",
   eager: true,
